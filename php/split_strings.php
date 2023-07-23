98c766a305f12a704c0ed9f2f2ac06d5b6acfe5e
Ovidiu Muresan <osilvium@gmail.com>
2023-07-23 17:39:00 +0200

    Fix katas commets

--- php/split_strings.php
+++ php/split_strings.php
@@ -1,16 +1,13 @@
 <?php
 
-/**
- *
-Split Strings
+// <6 kyu> Split Strings
 
-Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
 
-Examples:
+// Examples:
 
-* 'abc' =>  ['ab', 'c_']
-* 'abcdef' => ['ab', 'cd', 'ef']
- */
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
 
 
 function solution($str) {
--- php/two_one.php
+++ php/two_one.php
@@ -1,5 +1,7 @@
 <?php
 
+// <7 kyu> Two to One
+
 // Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
 
 // Examples:
